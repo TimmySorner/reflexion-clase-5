77dea503ea6d82874f2cb241e45bd52f1b8712d0
Timmy Sorner <juan.serna7836@unaula.edu.com>
2025-03-16 00:55:53 -0500

    se sube la última actualización

--- src/main/java/com/dianbridge/Libro.java
+++ src/main/java/com/dianbridge/Libro.java
@@ -2,4 +2,71 @@ package com.dianbridge;
 
 public class Libro {
 
+     // Atributos: título, autor, ISBN, año de publicación, disponible (boolean)
+
+     private String titulo;
+     private String autor;
+     private String isbn;
+     private String anoDePublicacion;
+     private boolean disponible;
+
+    // Constructor(es)
+
+    public Libro (String titulo, String autor, String ISBN, String anoDePublicacion, boolean disponible){
+
+    this.titulo = titulo;
+    this.autor = autor;
+    this.ISBN = ISBN;
+    this.anoDePublicacion = anoDePublicacion;
+    this.disponible = disponible;}
+
+    // Métodos getter y setter
+
+    public String getTitulo() {
+        return titulo;
+    }
+    
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getIsbn (){
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getAnoDePublicacion () {
+        return anoDePublicacion;
+    }
+
+    public void setAnoDePublicacion(String anoDePublicacion){
+        this.anoDePublicacion;
+    }
+
+    public boolean getDisponible() {
+        return disponible;
+    }
+
+    public void setAnoDePublicacion(boolean disponible){
+        this.Disponible;
+    }
+
+    
+
+
+
+
+
+
+
+
+    // Método para marcar como prestado
+
+    // Método para marcar como disponible
+
+    // Método toString() para mostrar información del libro
+
 }
--- src/main/java/com/dianbridge/Prestamo.java
+++ src/main/java/com/dianbridge/Prestamo.java
@@ -2,4 +2,73 @@ package com.dianbridge;
 
 public class Prestamo {
 
+// Atributos 
+
+
+ private Libro libro;
+ private Usuario usuario;
+ private String fechaPrestamo;
+ private String fechaDevolucion;
+
+
+// Constructor 
+
+ public Prestamo (String libro, String usuario, String fechaPrestamo, String fechaDevolucion) {
+
+
+    this.Libro = libro;
+    this.usuario = usuario;
+    this.fechaPrestamo = fechaPrestamo;
+    this.fechaDevolucion = fechaDevolucion;
+ }
+
+ // Metodos getter y setter
+
+ public String getlibro() {
+    return libro;
+ }
+
+public void setlibro(String libro) {
+    this.libro = libro;
+}
+
+ public String getusuario() {
+    return usuario;
+}
+
+public void setusuario(String usuario) {
+    this.usuario = usuario;
+}
+
+
+
+public String getfechaPrestamo() {
+    return fechaPrestamo;
+
+}
+
+public void setfechaPrestamo(String fechaPrestamo){
+    this.fechaPrestamo = fechaPrestamo;
+}
+
+
+public String getfechaDevolucion() {
+    return fechaDevolucion;
+}
+
+public void setfechaDevolucion(String fechaDevolucion){
+    this.fechaDevolucion= fechaDevolucion;
+}
+
+// Metodo para calcular dias de retraso  
+
+public void calcularRetraso()   {
+ 
+
+
+}
+
+
+
+
 }
